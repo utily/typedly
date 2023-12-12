e40ed556c3db13f7430de4858be9713e5408f798
Elias Eriksson <eliaseriksson95@gmail.com>
2023-12-12 09:54:32 +0100

    smaller import

--- typely.ts
+++ typely.ts
@@ -1,5 +1,5 @@
 /**
  * make sure to export new types in this file and ./global.ts
  */
-export { DistributiveOmit } from "./Object"
-export { Return, Parameter } from "./Function"
+export * from "./Object"
+export * from "./Function"
