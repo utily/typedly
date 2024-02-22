822ff30af6f6fb6968bfeb3b088a7d42a4f6c0d8
Elias Eriksson <elias@issuefab.com>
2024-02-22 14:36:14 +0100

    typely -> typedly (#2)

--- Object.spec.ts
+++ Object.spec.ts
@@ -1,11 +1,11 @@
-import { typely } from "./index"
+import { typedly } from "./index"
 
 describe("Object", () => {
 	it("DistributiveOmit", () => {
 		type A = { a: string; common: number }
 		type B = { b: boolean; common: number }
 		type Union = A | B
-		type CommonOmitted = typely.DistributiveOmit<Union, "common">
+		type CommonOmitted = typedly.DistributiveOmit<Union, "common">
 
 		const result: CommonOmitted = {
 			a: "hello world",
