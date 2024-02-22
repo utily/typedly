d8353efe007aa458d912428ee439dcbf7b55a253
Elias Eriksson <eliaseriksson95@gmail.com>
2024-02-22 13:33:46 +0100

    implemented ProtectedConstructorParameters

--- Function.ts
+++ Function.ts
@@ -1,2 +1,4 @@
 export type Return<T extends (...parameters: any[]) => any> = Awaited<ReturnType<T>>
 export type Parameter<T extends (...parameters: any[]) => any, N extends number> = Parameters<T>[N]
+// source: https://github.com/microsoft/TypeScript/issues/31278#issuecomment-686070769
+export type ProtectedConstructorParameters<T> = ConstructorParameters<(new (...args: any) => { [x: string]: any }) & T>
