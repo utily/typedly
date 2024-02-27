da03445ec7a82b36d4887eaefd8fc378fd284c4a
Elias Eriksson <elias@issuefab.com>
2024-02-27 10:58:22 +0100

    Fixed global types (#4)
    * repaired global types
    
    * testing both global and namespace import
    
    * 1 + 1 = 2

--- Function.spec.ts
+++ Function.spec.ts
@@ -1,18 +1,20 @@
+import "./global"
 import { typedly } from "./index"
 
 describe("Function", () => {
 	it("Return", () => {
 		function work(): number {
-			const result: typedly.Return<typeof work> = 1
-			return result
+			const namespace: typedly.Return<typeof work> = 1
+			const global: Return<typeof work> = 1
+			return namespace + global
 		}
-		expect(work()).toEqual(1)
+		expect(work()).toEqual(2)
 	})
 	it("Parameter", () => {
 		function foo(_: string, __: number): boolean {
 			return true
 		}
-		function bar(a: typedly.Parameter<typeof foo, 0>, b: typedly.Parameter<typeof foo, 1>): string {
+		function bar(a: typedly.Parameter<typeof foo, 0>, b: Parameter<typeof foo, 1>): string {
 			return a + b.toString(10)
 		}
 		expect(bar("a", 1)).toEqual("a1")
@@ -34,7 +36,7 @@ describe("Function", () => {
 			protected constructor(foo: string) {}
 		}
 		class Implementation extends Base {
-			constructor(...parameters: typedly.ProtectedConstructorParameters<typeof Base>) {
+			constructor(...parameters: ProtectedConstructorParameters<typeof Base>) {
 				super(...parameters)
 			}
 		}
--- Function.ts
+++ Function.ts
@@ -1,4 +1,6 @@
 export type Return<T extends (...parameters: any[]) => any> = Awaited<ReturnType<T>>
 export type Parameter<T extends (...parameters: any[]) => any, N extends number> = Parameters<T>[N]
 // source: https://github.com/microsoft/TypeScript/issues/31278#issuecomment-686070769
-export type ProtectedConstructorParameters<T> = ConstructorParameters<(new (...args: any) => { [x: string]: any }) & T>
+export type ProtectedConstructorParameters<T> = ConstructorParameters<
+	(new (...parameters: any[]) => { [x: string]: any }) & T
+>
