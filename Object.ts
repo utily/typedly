22b2b9d75d167df9bf15a6c59b6584a94a16747b
Elias Eriksson <eliaseriksson95@gmail.com>
2024-03-21 14:09:12 +0100

    simplified

--- Object.ts
+++ Object.ts
@@ -1,4 +1,4 @@
 export type DistributiveOmit<T, K extends keyof any> = T extends any ? Omit<T, K> : never
 export type DistributiveExclude<T, K extends keyof T, E> = {
-	[key in keyof T]: key extends K ? Exclude<Pick<T, K>[key], E> : T[key]
+	[key in keyof T]: key extends K ? Exclude<T[key], E> : T[key]
 }
